1c2f7675e2fba5e7de3b5149dcdf7a62587e3d3c
DhruvKudale <dkudale07@gmail.com>
2023-06-16 19:40:54 +0530

    Concatenation of table cells in same row and col

--- src/ocr_config.py
+++ src/ocr_config.py
@@ -1,7 +1,9 @@
 # Table OCR Related Properties
 faster_rcnn_model_path = '../../rcnn_model/model100.pth'
-det_threshold = 0.75
+det_threshold = 0.5
 table_recognition_language = 'eng'
+row_determining_threshold = 0.6667
+col_determining_threshold = 0.5
 
 # Server related directory paths
 output_dir = '../../output_books/'
--- src/table_cellwise_detection.py
+++ src/table_cellwise_detection.py
@@ -5,7 +5,7 @@ import glob as glob
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 import pytesseract
-from ocr_config import faster_rcnn_model_path, det_threshold, table_recognition_language
+from ocr_config import faster_rcnn_model_path, det_threshold, table_recognition_language, row_determining_threshold, col_determining_threshold
 
 
 # Returns Faster RCNN model to perfrom table cell-wise detection
@@ -94,13 +94,14 @@ def get_tables_from_page(img_file):
             else:
                 cells.append(boxes[i])
 
-
         for tablebbox in tables:
         
             tabcells = get_cells_from_table(tablebbox, cells)
+            # print(tablebbox)
+            # print(len(tabcells))
 
             # Proceed only if table has cells
-            if len(tabcells):
+            if len(tabcells) > 0:
                 # Sort cells based on y coordinates
                 strcells = sorted(tabcells, key=lambda b:b[1]+b[3], reverse=False)
 
@@ -113,7 +114,7 @@ def get_tables_from_page(img_file):
                 assign_row = 0
                 for i in range(len(strcells) - 1):
                     consec_cell_height = strcells[i + 1][1] - strcells[i][1]
-                    if consec_cell_height > 0.75 * mean_height:
+                    if consec_cell_height > row_determining_threshold * mean_height:
                         assign_row = assign_row + 1
                     cellrow.append(assign_row)
 
@@ -128,50 +129,96 @@ def get_tables_from_page(img_file):
                 # Generate row-wise cell sequences of bounding boxes
                 cellrows = {}
                 for i in rows:
-                    cells = []
+                    row_wise_cells = []
                     for j in range(len(strcells)):
                         if i == cellrow[j]:
-                            cells.append(strcells[j])
-                    cells = sorted(cells, key=lambda b:b[0], reverse=False)
-                    cellrows[i] = cells
+                            row_wise_cells.append(strcells[j])
+                    row_wise_cells = sorted(row_wise_cells, key=lambda b:b[0], reverse=False)
+                    cellrows[i] = row_wise_cells
 
                 tableresponse = {}
                 tableresponse['bbox'] = tablebbox 
                 tableresponse['nrows'] = nrows
-                tableresponse['ncols'] = ncols
                 tableresponse['ncells'] = len(strcells)
                 tableresponse['cellrows'] = cellrows
                 full_table_response.append(tableresponse)
-    
     return full_table_response
 
-def get_cell_text(image, cellbbox, i , j):
+def get_cell_text(image, cellbbox, lang):
     cropped_image = image[cellbbox[1]:cellbbox[3], cellbbox[0]:cellbbox[2]]
-    text = pytesseract.image_to_string(cropped_image, lang=table_recognition_language, config='--psm 6')
-    #cv2.imwrite(f"../DEMO/STRUC_ROW_" + str(i) + " " + "COL_" + str(j) + ".jpg", cropped_image)
+    text = pytesseract.image_to_string(cropped_image, lang=lang, config='--psm 6')
     return text
+
+def get_merged_cell(final_cells):
+    if len(final_cells) == 1:
+        return final_cells[0]
+    x1 = [c[0] for c in final_cells]
+    y1 = [c[1] for c in final_cells]
+    x2 = [c[2] for c in final_cells]
+    y2 = [c[3] for c in final_cells]
+    cell = [min(x1), min(y1), max(x2), max(y2)]
+    return cell
+
+def get_final_cell(tablecellrows, skeleton, rowindex, colindex):
+    final_cells = []
+    row_to_consider = tablecellrows[rowindex]
+    col_skeleton = skeleton[rowindex]
+    assert len(row_to_consider) == len(col_skeleton)
+    for i in range(len(col_skeleton)):
+        if col_skeleton[i] == colindex:
+            final_cells.append(row_to_consider[i])
+    if len(final_cells) == 0:
+        return []
+    else:
+        final_cell = get_merged_cell(final_cells)
+    return final_cell
 
 def get_hocr_from_table_response(imgfile, tableresponse):
     raw_image = cv2.imread(imgfile)
-    tablebbox = tableresponse['bbox']
-    tablerows = tableresponse['nrows']
-    tablecols = tableresponse['ncols']
+    nrows = tableresponse['nrows']
     tablecellrows = tableresponse['cellrows']
+    tablebbox = tableresponse['bbox']
+    lang = table_recognition_language
+
+    # Preparing skeleton to assign column numbers
+    final_skeleton = []
+    max_entries_per_row = []
+    for row in tablecellrows:
+        row_to_consider = tablecellrows[row]
+        # Calculate Mean height
+        cell_widths = [c[3] - c[1] for c in row_to_consider]
+        mean_width = int(np.mean(cell_widths))
+        # Sort cells in same row from left to right
+        ltor_cells = sorted(row_to_consider, key=lambda b:b[0], reverse=False)
+        # Assign col number to every cell
+        col_to_assign = 0
+        assigned_col = [0]
+        for i in range(len(ltor_cells) - 1):
+            consec_cell_diff = ltor_cells[i + 1][0] - ltor_cells[i][0]
+            if consec_cell_diff > col_determining_threshold * mean_width:
+                col_to_assign = col_to_assign + 1
+            assigned_col.append(col_to_assign)
+        max_entries_per_row.append(col_to_assign)
+        final_skeleton.append(assigned_col)
+        
+    ncols = max(max_entries_per_row)  + 1
+    # print(final_skeleton)
+    # print("Rows => " + str(nrows))
+    # print("Cols => " + str(ncols))
+
+    # HOCR Generation
     hocr = '<table class="ocr_tab" border=1 style="margin: 0px auto; text-align: center;"'
     tabbbox = " ".join(tablebbox.astype(str))
     hocr = hocr + f' title = "bbox {tabbbox}" >'
-    for i in range(tablerows):
+    for i in range(nrows):
         hocr = hocr + '<tr>'
-        rowdata = tablecellrows[i]
-        for j in range(tablecols):
-            hocr = hocr + '<td'
-            try:
-                bbox = " ".join(rowdata[j].astype(str))
-                hocr = hocr + f' title = "bbox {bbox}" >'
-                hocr = hocr + get_cell_text(raw_image, rowdata[j], i, j)
-            except:
-                hocr = hocr + '> '
-            hocr = hocr + '</td>' 
+        for j in range(ncols):
+            cell = get_final_cell(tablecellrows, final_skeleton, i, j)
+            if len(cell) == 0:
+                text = ''
+            else :
+                text = get_cell_text(raw_image, cell, lang)
+            hocr = hocr + '<td>' + text + '</td>' 
         hocr = hocr + '</tr>'
     hocr = hocr + '</table>'
     entry = []
@@ -187,5 +234,5 @@ def get_final_table_hocrs_from_image(imgfile):
         full_hocrs_response.append(hocr)
     return full_hocrs_response
 
-# img_file = '../DEMO/10.jpg'
-# print(get_final_table_hocrs_from_image(img_file))
+img_file = '../DEMO/10.jpg'
+print(get_final_table_hocrs_from_image(img_file))
